f1bc8ed53d54338568107387104630a86d8f922f
sserges <agounkeserge@gmail.com>
2019-11-19 16:38:09 +0000

    Practical API Module

---
+++ src/updates/api/views.py
@@ -0,0 +1,20 @@
+from updates.models import Update as UpdateModel
+
+
+class UpdateModelDetailAPIView():
+    def get(self, request, *args, **kwargs):
+        return
+    
+    def post(self, request, *args, **kwargs):
+        return
+    
+    def put(self, request, *args, **kwargs):
+        return
+    
+    def delete(self, request, *args, **kwargs):
+        return
+
+
+class UpdateModelListAPIView():
+    def get(self, request, *args, **kwargs):
+        return
