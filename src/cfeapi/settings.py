7445f3afd9b5bcab40a3c43343a7b1dbaffb4f03
sserges <agounkeserge@gmail.com>
2019-11-21 16:02:12 +0000

    Practical API Module Part 2

--- src/cfeapi/settings.py
+++ src/cfeapi/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# APPEND_TRAILING_SLASH = False
 
 # Application definition
 
--- src/updates/models.py
+++ src/updates/models.py
@@ -23,7 +23,7 @@ class UpdateQuerySet(models.QuerySet):
 
 
     def serialize(self):
-        list_values = list(self.values("user", "content", "image"))
+        list_values = list(self.values("id", "user", "content", "image"))
         print(list_values)
         return json.dumps(list_values)
 
@@ -58,6 +58,7 @@ class Update(models.Model):
             image = ""
         
         data = {
+            "id": self.id,
             "content": self.content,
             "user": self.user.id,
             "image": image
