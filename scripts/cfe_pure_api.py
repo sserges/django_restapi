923f236a567a1fa5c8091268f73fbe85f35f0e93
sserges <agounkeserge@gmail.com>
2019-11-22 17:14:06 +0000

    Validating Data with a Model Form

--- scripts/cfe_pure_api.py
+++ scripts/cfe_pure_api.py
@@ -8,21 +8,21 @@ ENDPOINT = "api/updates/"
 def get_list():
     r = requests.get(BASE_URL + ENDPOINT)
     data = r.json()
-    for obj in data:
-        print(obj['id'])
-        if obj['id'] == 1:
-            r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
-            print(dir(r2))
-            print(r2.json())
+    # for obj in data:
+    #     print(obj['id'])
+    #     if obj['id'] == 1:
+    #         r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
+    #         print(dir(r2))
+    #         print(r2.json())
     return data
 
 
 def create_update():
     new_data = {
         'user': 1,
-        'content': 'Another new cool update'
+        'content': 'Another Some more cool content'
     }
-    r = requests.post(BASE_URL + ENDPOINT, data=new_data)
+    r = requests.post(BASE_URL + ENDPOINT + "1/", data=new_data)
     print(r.status_code)
     if r.status_code == requests.codes.ok:
         # print(r.json())
--- src/cfeapi/settings.py
+++ src/cfeapi/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# APPEND_TRAILING_SLASH = False
+# APPEND_SLASH=True
 
 # Application definition
 
