3e7bade70342a5f898bd9d7282e0dad075de26f3
sserges <agounkeserge@gmail.com>
2019-12-03 16:26:46 +0000

    Detail API View

--- src/status/api/views.py
+++ src/status/api/views.py
@@ -39,4 +39,16 @@ class StatusCreateAPIView(generics.CreateAPIView):
 
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
-    
+
+
+class StatusDetailAPIView(generics.RetrieveAPIView):
+    permission_classes     = []
+    authentication_classes = []
+    queryset               = Status.objects.all()
+    serializer_class       = StatusSerializer
+    # lookup_field           = 'id'
+
+    # def get_object(self, *args, **kwargs):
+    #     kwargs = self.kwargs
+    #     kw_id = kwargs.get('id')
+    #     return Status.objects.get(id=kw_id)
