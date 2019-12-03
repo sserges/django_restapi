d7de26446c77aeec2db1fa727698ad464a70480b
sserges <agounkeserge@gmail.com>
2019-12-03 15:47:08 +0000

    Create API View

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
 
-from .views import StatusListSearchAPIView, StatusAPIView
+from .views import (
+    StatusListSearchAPIView, 
+    StatusAPIView,
+    StatusCreateAPIView
+)
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
-    # url(r'^create/$', StatusCreateAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
     # url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
     # url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
     # url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
