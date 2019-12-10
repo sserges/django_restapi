e82bb3421a53e047bd233a8b5abdd3c94f045225
sserges <agounkeserge@gmail.com>
2019-12-10 10:19:47 +0000

    Uploading & Handling Images

--- scripts/cfe_rest_framework_api.py
+++ scripts/cfe_rest_framework_api.py
@@ -1,8 +1,32 @@
-import json
+import json, os
 
 import requests
 
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
+
+image_path = os.path.join(os.getcwd(), "readyplayeronejpg.jpg")
+
+
+def do_img(method='get', data={}, is_json=True, img_path=None):
+    headers = {}
+    if is_json:
+        headers['content-type'] = 'application/json'
+        data = json.dumps(data)
+    if img_path is not None:
+        with open(img_path, 'rb') as image:
+            file_data = {
+                'image': image
+            }
+            r = requests.request(method, ENDPOINT, data=data, files=file_data, headers=headers)
+    else:
+        r = requests.request(method, ENDPOINT, data=data, headers=headers)
+    print(r.text)
+    print(r.status_code)
+    return r
+
+
+do_img(method='put', data={"id": 14, "user": 1, "content": "Some new content"}, is_json=False, img_path=image_path)
+
 
 
 def do(method='get', data={}, is_json=True):
@@ -18,7 +42,7 @@ def do(method='get', data={}, is_json=True):
 
 # do(data={'id':40})
 
-do(method='delete' ,data={'id':9})
+# do(method='delete' ,data={'id':9})
 
 # do(method='put' ,data={'id':1, 'content': 'some cool new content', 'user': 1})
 
